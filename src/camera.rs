884c618d72c2b51c4099d29b62d415ef8067f21b
gak <gak@gak0.com>
2022-08-20 16:59:45 +1000

    fix: rmb pan speed

--- src/camera.rs
+++ src/camera.rs
@@ -27,37 +27,37 @@ pub fn move_camera(
     mut scroll_events: EventReader<MouseWheel>,
     mut camera_query: Query<(&mut Transform, &mut GameCamera), With<Camera>>,
 ) {
-    let mut pan = Vec2::ZERO;
-
-    // Keyboard pan.
-    if input.pressed(KeyCode::A) || input.pressed(KeyCode::Left) {
-        pan.x -= settings.keyboard_scroll_speed;
-    }
-    if input.pressed(KeyCode::D) || input.pressed(KeyCode::Right) {
-        pan.x += settings.keyboard_scroll_speed;
-    }
-    if input.pressed(KeyCode::W) || input.pressed(KeyCode::Up) {
-        pan.y -= settings.keyboard_scroll_speed;
-    }
-    if input.pressed(KeyCode::S) || input.pressed(KeyCode::Down) {
-        pan.y += settings.keyboard_scroll_speed;
-    }
-
-    // Mouse pan.
-    if buttons.pressed(MouseButton::Right) {
-        for mouse_motion in motion_events.iter() {
-            pan -= mouse_motion.delta;
-        }
-    }
-
-    let mut zoom = 0f32;
-
-    // Mouse zoom.
-    for mouse_wheel in scroll_events.iter() {
-        zoom -= mouse_wheel.y * settings.mouse_scroll_speed;
-    }
-
     for (mut camera_transform, mut game_camera) in camera_query.iter_mut() {
+        let mut pan = Vec2::ZERO;
+
+        // Keyboard pan.
+        if input.pressed(KeyCode::A) || input.pressed(KeyCode::Left) {
+            pan.x -= settings.keyboard_scroll_speed;
+        }
+        if input.pressed(KeyCode::D) || input.pressed(KeyCode::Right) {
+            pan.x += settings.keyboard_scroll_speed;
+        }
+        if input.pressed(KeyCode::W) || input.pressed(KeyCode::Up) {
+            pan.y -= settings.keyboard_scroll_speed;
+        }
+        if input.pressed(KeyCode::S) || input.pressed(KeyCode::Down) {
+            pan.y += settings.keyboard_scroll_speed;
+        }
+
+        // Mouse pan.
+        if buttons.pressed(MouseButton::Right) {
+            for mouse_motion in motion_events.iter() {
+                pan -= (game_camera.zoom.sqrt() / 3.0) * mouse_motion.delta
+            }
+        }
+
+        let mut zoom = 0f32;
+
+        // Mouse zoom.
+        for mouse_wheel in scroll_events.iter() {
+            zoom -= mouse_wheel.y * settings.mouse_scroll_speed;
+        }
+
         game_camera.zoom += zoom * game_camera.zoom * time.delta_seconds();
         if game_camera.zoom < 10.0 {
             game_camera.zoom += game_camera.zoom * 3.0 * time.delta_seconds();
