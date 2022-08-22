f82dd5e890de1863825ba80eebd1a0cff65d2faf
gak <gak@gak0.com>
2022-08-22 13:00:15 +1000

    wip: defs

--- shared/src/game/defs.rs
+++ shared/src/game/defs.rs
@@ -19,9 +19,8 @@ pub struct Defs {
 
 impl Defs {
     pub fn load() -> Self {
-        let static_data_path = include_str!("../../../game/assets/defs.yaml");
-        let static_data_str = std::fs::read_to_string(static_data_path).unwrap();
-        serde_yaml::from_str(&static_data_str).unwrap()
+        let data = include_str!("../../../game/assets/defs.yaml");
+        serde_yaml::from_str(data).unwrap()
     }
 
     // This has to be run from the game directory!
