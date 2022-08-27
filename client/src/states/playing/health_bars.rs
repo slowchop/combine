c658806c8ae7add561aff75e7046a25d84d367e2
gak <gak@gak0.com>
2022-08-27 20:38:46 +1000

    feat: health bars

--- client/src/states/playing/health_bars.rs
+++ client/src/states/playing/health_bars.rs
@@ -13,18 +13,18 @@ pub fn add_health_bars(
     mut commands: Commands,
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
-    query: Query<(Entity, &Transform), (With<Damaged>, Without<HasHealthBar>)>,
+    query: Query<(Entity, &Transform), (With<CreepRef>, With<Damaged>, Without<HasHealthBar>)>,
 ) {
     for (entity, transform) in query.iter() {
-        println!("No health bar, spawning");
+        println!("No health bar, spawning for {:?}", entity);
         let mesh = meshes.add(shape::Quad::new(Vec2::new(1.0, 0.2)).into());
-        let material = materials.add(StandardMaterial::from(Color::rgba(0.0, 1.0, 0.0, 0.0)));
+        let material = materials.add(StandardMaterial::from(Color::rgba(1.0, 0.0, 0.0, 1.0)));
 
         commands.entity(entity).insert(HasHealthBar);
         commands
             .spawn_bundle(MaterialMeshBundle {
                 material,
-                transform: transform.with_translation(Vec3::new(0.0, 0.5, 1.0)),
+                transform: *transform,
                 mesh,
                 ..Default::default()
             })
@@ -36,26 +36,33 @@ pub fn health_bars(
     mut commands: Commands,
     defs: Res<Defs>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    creep_query: Query<(
-        &Damaged,
-        &CreepRef,
-        &mut Transform,
-        &Handle<StandardMaterial>,
-    )>,
-    mut health_bar_query: Query<(Entity, &mut Transform, &HealthBar), Without<CreepRef>>,
+    creep_query: Query<(&Damaged, &Transform, &CreepRef), With<HasHealthBar>>,
+    mut health_bar_query: Query<
+        (
+            Entity,
+            &mut Transform,
+            &HealthBar,
+            &mut Handle<StandardMaterial>,
+        ),
+        Without<Damaged>,
+    >,
 ) {
-    for (health_bar_entity, mut health_bar_transform, health_bar) in health_bar_query.iter_mut() {
-        let (damaged, creep_ref, &transform, material) =
-            if let Ok(c) = creep_query.get(health_bar.0) {
-                c
-            } else {
-                warn!(
-                    "Entity not found in creep_query for health_bars: {:?} Despawning. It's cool.",
-                    health_bar.0
-                );
-                commands.entity(health_bar_entity).despawn();
-                continue;
-            };
+    // Health bar query seems to work.
+    // Just can't access the creep entity.
+
+    for (health_bar_entity, mut health_bar_transform, health_bar, material) in
+        health_bar_query.iter_mut()
+    {
+        let (damaged, &transform, creep_ref) = if let Ok(c) = creep_query.get(health_bar.0) {
+            c
+        } else {
+            warn!(
+                "Entity not found in creep_query for health_bars: {:?} Despawning. It's cool.",
+                health_bar.0
+            );
+            commands.entity(health_bar_entity).despawn();
+            continue;
+        };
 
         let creep = if let Some(creep) = defs.creep(&creep_ref) {
             creep
@@ -64,9 +71,10 @@ pub fn health_bars(
             continue;
         };
 
-        // From green to red.
-        let fraction = damaged.0 as f32 / creep.health as f32;
-        materials.get_mut(material).unwrap().base_color = Color::rgb(1.0 - fraction, fraction, 0.0);
+        let fraction = 1.0 - damaged.0 as f32 / creep.health as f32;
+        let material = materials.get_mut(&material).unwrap();
+        material.base_color = Color::rgb(1.0 - fraction, fraction, 1.0);
+
         health_bar_transform.scale.x = fraction;
     }
 }
