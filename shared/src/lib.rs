5007b0472bd93a9910ea1146a8e37e3d96288ff8
gak <gak@gak0.com>
2022-08-28 12:00:11 +1000

    config: 1 minute cycle

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -47,14 +47,14 @@ pub const TICKS_PER_SECOND: u64 = 1000 / MS_PER_TICK;
 // pub const RESPAWN_CLOCK_TIME: Ticks = Ticks(20 * TICKS_PER_SECOND as i64);
 // pub const RELEASE_CLOCK_TIME: Ticks = Ticks(15 * TICKS_PER_SECOND as i64);
 
-pub const TICKS_PER_DAY: Ticks = Ticks(30 * TICKS_PER_SECOND as i64);
+pub const TICKS_PER_DAY: Ticks = Ticks(60 * TICKS_PER_SECOND as i64);
 pub const RESPAWN_CLOCK_TIME: Ticks = Ticks(1 * TICKS_PER_SECOND as i64);
-pub const RELEASE_CLOCK_TIME: Ticks = Ticks(20 * TICKS_PER_SECOND as i64);
+pub const RELEASE_CLOCK_TIME: Ticks = Ticks(31 * TICKS_PER_SECOND as i64);
 
 pub fn shared_config() -> SharedConfig<Channels> {
     let tick_interval = Some(Duration::from_millis(MS_PER_TICK as u64));
-    let link_condition = None;
-    // let link_condition = Some(LinkConditionerConfig::poor_condition());
+    // let link_condition = None;
+    let link_condition = Some(LinkConditionerConfig::poor_condition());
 
     SharedConfig::new(
         SocketConfig::new(link_condition, None),
